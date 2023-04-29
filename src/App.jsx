50f8cf968ccd9cf8e219dd48c8d95fbb569b1ccc
Jerome Jalandoon <jbjalandoon>
2023-04-29 19:52:03 +0800

    add page transition

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,75 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [currentView, setCurrentView] = useState("home");
+  const views = ["home", "about me", "projects", "contact me"];
+  let animationClass = "animate-slide-left";
+  const [currentView, setCurrentView] = useState({
+    index: 0,
+    animationClass: "animate-slide-left",
+  });
+
+  const changeViewHandler = (e) => {
+    const index = views.findIndex(
+      (el) => el === e.currentTarget.innerHTML.toLowerCase()
+    );
+    if (currentView.index > index) animationClass = "animate-slide-right";
+    setCurrentView({ index, animationClass });
+  };
 
   return (
-    <main className='flex w-full flex-col items-center h-screen'>
+    <main className='flex w-full flex-col pt-14 items-center h-screen'>
       <header className='w-full flex flex-col items-center justify-center max-w-4xl'>
         <span className='text-3xl py-3'>Jerome Jalandoon</span>
         <nav className='py-3'>
-          <ul className='flex gap-8 text-lg sm:flex-row flex-col underline-offset-4 transition-all'>
-            <li className='nav-link'>Home</li>
-            <li className='nav-link'>About Me</li>
-            <li className='nav-link'>Projects</li>
-            <li className='nav-link'>Contact Me</li>
+          <ul className='flex gap-8 text-lg sm:flex-row flex-col underline-offset-4 transition-all select-none'>
+            <li
+              className={`nav-link ${currentView.index === 0 && "active"}`}
+              onClick={changeViewHandler}>
+              Home
+            </li>
+            <li
+              className={`nav-link ${currentView.index === 1 && "active"}`}
+              onClick={changeViewHandler}>
+              About Me
+            </li>
+            <li
+              className={`nav-link ${currentView.index === 2 && "active"}`}
+              onClick={changeViewHandler}>
+              Projects
+            </li>
+            <li
+              className={`nav-link ${currentView.index === 3 && "active"}`}
+              onClick={changeViewHandler}>
+              Contact Me
+            </li>
           </ul>
         </nav>
       </header>
-      <div className='w-full py-8 max-w-7xl mt-7 h-full'>
-        <div className='border w-full rounded-3xl h-full'></div>
+      <div className='w-full md:max-w-4xl mt-7 my-10 lg:px-0 h-full '>
+        {currentView.index === 0 && (
+          <div
+            className={`border w-full rounded-3xl h-full ${currentView.animationClass}`}>
+            Home
+          </div>
+        )}
+        {currentView.index === 1 && (
+          <div
+            className={`border w-full rounded-3xl h-full ${currentView.animationClass}`}>
+            About
+          </div>
+        )}
+        {currentView.index === 2 && (
+          <div
+            className={`border w-full rounded-3xl h-full ${currentView.animationClass}`}>
+            Projects
+          </div>
+        )}
+        {currentView.index === 3 && (
+          <div
+            className={`border w-full rounded-3xl h-full ${currentView.animationClass}`}>
+            Something
+          </div>
+        )}
       </div>
     </main>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        "slide-left": {
+          "0%": { transform: "translateX(200%)", opacity: "0%" },
+          "100%": { transform: "translateX(0)", opacity: "100%" },
+        },
+        "slide-right": {
+          "0%": { transform: "translateX(-200%)", opacity: "0%" },
+          "100%": { transform: "translateX(0)", opacity: "100%" },
+        },
+      },
+      animation: {
+        "slide-left": "slide-left 1250ms ease",
+        "slide-right": "slide-right 1250ms ease",
+      },
+    },
   },
   plugins: [],
 };
