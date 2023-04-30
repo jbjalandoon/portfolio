972ded6d4af4e84589597436edd1ecd515754331
Jerome Jalandoon <jbjalandoon>
2023-05-01 02:44:04 +0800

    home page

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,5 +1,7 @@
 import { useContext } from "react";
 import developer from "../../assets/developer.svg";
+import github from "../../assets/github.svg";
+import linkedin from "../../assets/linkedin.svg";
 import Card from "./Card";
 import { ViewContext } from "../../store/ViewContext";
 
@@ -7,14 +9,31 @@ export default function Content() {
   const { view } = useContext(ViewContext);
 
   return (
-    <div className='w-full md:max-w-4xl mt-7 my-10 lg:px-0 h-full'>
+    <div className='w-full md:max-w-6xl mt-7 my-10 lg:px-0 h-full'>
       {view === 0 && (
         <Card>
           <div className='flex md:flex-row flex-col text-center md:gap-10 gap-16 justify-center items-center'>
-            <div className='flex-col flex'>
-              <span className='text-3xl'>Full-Stack Developer</span>
-              <span>Hello, I'm Jerome a junior full-stack developer</span>
-              <span></span>
+            <div className='flex-col flex gap-4'>
+              <span className='text-6xl'>Full-Stack Developer</span>
+              <span className='text-2xl'>
+                Hello, I'm Jerome a junior full-stack developer
+              </span>
+              <span className='text-2xl'>
+                Check out my,{" "}
+                <a
+                  href='https://github.com/jbjalandoon'
+                  target='_blank'
+                  className='underline underline-offset-8'>
+                  Github
+                </a>{" "}
+                and{" "}
+                <a
+                  href='https://www.linkedin.com/in/jbjalandoon/'
+                  target='_blank'
+                  className='underline underline-offset-8'>
+                  Linkedin
+                </a>
+              </span>
             </div>
             <div>
               <img src={developer} className='h-40 w-full object-cover' />
