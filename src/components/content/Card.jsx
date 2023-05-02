59b72faec0af82abbf300031746d635e85e8562d
Jerome Jalandoon <jbjalandoon>
2023-05-02 14:28:27 +0800

    refactor message in home

--- src/components/content/Card.jsx
+++ src/components/content/Card.jsx
@@ -5,7 +5,7 @@ export default function Card({ children, className }) {
   const { animationClass } = useContext(ViewContext);
   return (
     <div
-      className={`w-full rounded-3xl h-full flex flex-col items-center justify-center p-12 ${className} ${animationClass} relative`}>
+      className={`w-full rounded-3xl h-full flex flex-col px-20 py-20 ${className} ${animationClass}`}>
       {children}
     </div>
   );
--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -9,16 +9,20 @@ export default function Content() {
   const { view } = useContext(ViewContext);
 
   return (
-    <div className='w-full md:max-w-6xl mt-7 my-10 lg:px-0 h-full'>
+    <div className='w-full md:max-w-6xl mt-7 lg:px-0 h-full'>
       {view === 0 && (
         <Card>
-          <div className='flex md:flex-row flex-col text-center md:gap-10 gap-16 justify-center items-center'>
+          <div className='flex md:flex-row flex-col md:text-left text-center md:gap-10 gap-16 items-center'>
             <div className='flex-col flex gap-4'>
-              <span className='text-6xl'>Full-Stack Developer</span>
-              <span className='text-2xl'>
-                Hello, I'm Jerome a junior full-stack developer
-              </span>
-              <span className='text-2xl'>
+              <span className='text-xl'>Hello, my name is</span>
+              <span className='text-7xl'>Jerome Jalandoon.</span>
+              <span className='text-7xl'>I'm a Web Developer.</span>
+              <p className='text-lg w-full md:w-1/2'>
+                I'm a graduating student in Polytechnic University of the
+                Philippines Taguig (2023). I specialize in Fullstack
+                Development. I'm currently focused in building my own projects.
+              </p>
+              {/* <span className='text-2xl'>
                 Check out my,{" "}
                 <a
                   href='https://github.com/jbjalandoon'
@@ -33,10 +37,7 @@ export default function Content() {
                   className='underline underline-offset-8'>
                   Linkedin
                 </a>
-              </span>
-            </div>
-            <div>
-              <img src={developer} className='h-40 w-full object-cover' />
+              </span> */}
             </div>
           </div>
         </Card>
