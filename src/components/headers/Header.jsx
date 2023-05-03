91f11cf799def78715f7d064b13310e94f6bef84
Jerome Jalandoon <jbjalandoon>
2023-05-03 14:58:48 +0800

    responsive header

--- src/components/headers/Header.jsx
+++ src/components/headers/Header.jsx
@@ -1,11 +1,34 @@
+import { useState } from "react";
 import Nav from "./Nav";
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
 import Title from "./Title";
 
 export default function Header({ items, title }) {
+  const [showHeader, setShowHeader] = useState(false);
+
+  const showHeaderHandler = () => {
+    setShowHeader((prevState) => !prevState);
+  };
+
   return (
-    <header className='w-full md:flex-row sm:flex-col flex-col flex h-20 items-center justify-center max-w-4xl relative'>
-      {/* <Title>{title}</Title> */}
-      <Nav items={items} />
-    </header>
+    <>
+      <header
+        className={`w-1/2 md:left-auto left-1/2 md:animate-none shadow-xl md:shadow-none md:w-full md:flex-row flex-col z-50 md:bg-transparent bg-slate-950 flex md:h-20 h-screen items-center justify-center md:inline-flex max-w-4xl md:relative ${
+          showHeader ? "absolute" : "hidden"
+        } md:top-auto top-0`}>
+        {/* <Title>{title}</Title> */}
+        <button
+          className='absolute top-8 p-3 md:hidden block'
+          onClick={showHeaderHandler}>
+          X
+        </button>
+        <Nav items={items} />
+      </header>
+      {!showHeader && <button
+        className='md:hidden absolute text-7xl top-7 right-10'
+        onClick={showHeaderHandler}>
+        <HiOutlineMenuAlt3 />
+      </button>}
+    </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ViewContextProvider>
       <App />
-      <div className='absolute h-12 bg-red-600 w-full top-0 left-0 items-center justify-center bg-opacity-60 flex'>
+      <div className='absolute h-12 bg-red-600 w-full bottom-0 items-center justify-center bg-opacity-20 flex'>
         <span className='opacity-100'>
           This website is currently on development.
         </span>
