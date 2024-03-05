f0c2617447e21585ac2ba91ab98286c43aa42324
jbjalandoon <jerome.jalandoon@gmail.com>
2024-03-05 23:52:28 +0800

    refactor contact component

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,7 +1,6 @@
 import emailjs from "@emailjs/browser";
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import { createPortal } from "react-dom";
-import { FaPhoneAlt, FaRegAddressBook, FaEnvelope } from "react-icons/fa";
 import Alert from "./Alert";
 
 export default function Contacts() {
@@ -22,7 +21,7 @@ export default function Contacts() {
         import.meta.env.VITE_EMAIL_API_KEY
       );
       if (status !== 200) {
-        throw error();
+        throw new Error("Something went wrong");
       }
       setIsSuccess(true);
     } catch (error) {
@@ -34,45 +33,47 @@ export default function Contacts() {
 
   return (
     <>
-      <div className='g-4 flex w-full min-w-fit max-w-7xl flex-col gap-10'>
-        <div className='flex flex-col gap-1'>
-          <h1 className='mb-3 text-4xl'>Get in touch with me.</h1>
+      <div className="g-4 flex w-full min-w-fit max-w-7xl flex-col gap-10">
+        <div className="flex flex-col gap-1">
+          <h1 className="mb-3 text-4xl">Get in touch with me.</h1>
           <p>
-            I'm currently looking for a job opportunities, if you think I can
+            I&apos;m currently looking for a job opportunities, if you think I can
             contribute to your company. Send me a message!
           </p>
         </div>
-        <div className='flex h-full w-full flex-col justify-center gap-14 md:flex-row'>
+        <div className="flex h-full w-full flex-col justify-center gap-14 md:flex-row">
           <form
             ref={form}
             onSubmit={sendEmail}
-            className='flex w-full flex-col justify-center gap-4'>
-            <div className='grid grid-cols-1 gap-4 sm:grid-cols-2'>
+            className="flex w-full flex-col justify-center gap-4"
+          >
+            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
               <input
-                type='text'
-                name='from_name'
-                placeholder='Name'
-                className='h-14 w-full rounded-md bg-slate-800 px-3 text-slate-200'
+                type="text"
+                name="from_name"
+                placeholder="Name"
+                className="h-14 w-full rounded-md bg-slate-800 px-3 text-slate-200"
               />
               <input
-                type='email'
-                name='from_email'
-                placeholder='Email'
-                className='h-14 w-full rounded-md bg-slate-800 px-3 text-slate-200'
+                type="email"
+                name="from_email"
+                placeholder="Email"
+                className="h-14 w-full rounded-md bg-slate-800 px-3 text-slate-200"
               />
             </div>
             <div>
               <textarea
-                name='message'
-                className='h-24 w-full rounded-md bg-slate-800 px-3 pt-2 text-slate-200'
-                placeholder='Message'
+                name="message"
+                className="h-24 w-full rounded-md bg-slate-800 px-3 pt-2 text-slate-200"
+                placeholder="Message"
               />
             </div>
-            <div className='ml-auto flex w-full gap-4 md:w-60'>
+            <div className="ml-auto flex w-full gap-4 md:w-60">
               <button
-                type='submit'
-                className='relative w-full  cursor-pointer rounded-md border-2 border-green-700 bg-transparent py-2 font-bold transition-all duration-500 ease-in after:absolute after:left-0 after:top-0 after:-z-50 after:h-full after:w-0 after:bg-green-700 after:transition-all after:duration-1000 hover:text-black hover:after:w-full disabled:cursor-not-allowed'
-                disabled={isSending}>
+                type="submit"
+                className="relative w-full  cursor-pointer rounded-md border-2 border-green-700 bg-transparent py-2 font-bold transition-all duration-500 ease-in after:absolute after:left-0 after:top-0 after:-z-50 after:h-full after:w-0 after:bg-green-700 after:transition-all after:duration-1000 hover:text-black hover:after:w-full disabled:cursor-not-allowed"
+                disabled={isSending}
+              >
                 {isSending ? "Sending..." : "Send Message"}
               </button>
             </div>
@@ -83,7 +84,7 @@ export default function Contacts() {
         createPortal(
           <Alert
             message={
-              isSuccess ? "Message Successfully Sent" : "Message didn't sent"
+              isSuccess ? "Message Successfully Sent" : "Something went wrong"
             }
             type={isSuccess ? "success" : "danger"}
           />,
