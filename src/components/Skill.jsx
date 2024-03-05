de4661cefc32a8d55c852b354602b009d7c024dc
jbjalandoon <jerome.jalandoon@gmail.com>
2024-03-05 16:28:58 +0800

    add skill component

---
+++ src/components/Skill.jsx
@@ -0,0 +1,8 @@
+function Skill({ children }) {
+  return (
+    <span className="text-md rounded-2xl bg-slate-200 px-4 py-2 font-bold text-black">
+      {children}
+    </span>
+  );
+}
+export default Skill;
